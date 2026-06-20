ac2360274f264d5a1e03184c21f488f51ef5aee2
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-06-20 13:47:27 +0300

    update kde frameworks plasma versions

--- dev/qt/update-qt.sh
+++ dev/qt/update-qt.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 ver="$1"
-find . -type f -iname ympbuild | while read file ; do
+dir="$2"
+if [ -z "$dir" ] ; then
+    dir="."
+fi
+find "$dir" -type f -iname ympbuild | while read file ; do
     sed -i "s/version='.*'/version='$ver'/g" $file
     rel=$(bash -c "source $file &>/dev/null; echo \$release")
     rel=$(($rel+1))
